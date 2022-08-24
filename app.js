e9ef2bb3deb7917ec09d5bf7bd59431a3585d0d3
huna <2496337440@qq.com>
2022-08-24 20:16:08 +0800

    合并

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 //导入 express
+const path=require('path')
 const express = require("express");
 
 //创建服务器的实例对象
 const app = express();
-
 const joi = require("@hapi/joi");
-
 //导入并配置 cors 中间件
 const cors = require("cors");
 app.use(cors());
-
+app.use(express.json())
 //配置解析表单数据的中间件,注意：这个中间件只能解析application/x-www-form-urlencodedd 格式的表单数据
 app.use(express.urlencoded({ extended: false }));
-
+//处理静态资源
+app.use(express.static(path.join(__dirname,'uploads')))
 //一定要在路由之前封装 res.cc 函数
 app.use((req, res, next) => {
   //status 默认值为 1 ，表示失败的情况
-
   //err 的值，可能是一个错误对象，也可能是一个错误描述字符串
   res.cc = function (err, status = 1) {
     res.send({
@@ -27,11 +26,14 @@ app.use((req, res, next) => {
   next();
 });
 
+
 //导入并使用用户路由模块
 const userRouter = require("./router/user");
-
 app.use("/api", userRouter);
-
+//导入文章内容的路由模块
+ const artcat=require('./router/nav')
+ app.use('/api',artcat)
+ 
 //定义错误级别的中间件
 app.use((err, req, res, next) => {
   //验证失败导致的错误
@@ -43,5 +45,5 @@ app.use((err, req, res, next) => {
 
 //启动服务器
 app.listen(3007, () => {
-  console.log("服务器启动成功 baseUrl: htttp://127.0.0.1:3007");
+  console.log("服务器启动成功 baseUrl: http://127.0.0.1:3007");
 });
---
+++ router/nav.js
@@ -0,0 +1,23 @@
+// const path=require('path')
+const express = require("express");
+const router = express.Router();
+const upload = require("../utils/liad");
+//导入文章分类的模块
+const {
+  load,
+  lognav,
+  table,
+  delectinterface,
+  Modifyarticle,
+  article
+} = require("../router_handler/navvat");
+router.get("/nav", lognav).post("/ipload", upload.array("avatar", 12), load);
+router.post("/table", table);
+//删除文章分类的模块
+router.delete("/delectinterface", delectinterface);
+//修改文章内容
+router.post("/Modifyarticle", Modifyarticle);
+//获取文章内容
+router.get("/article", article);
+
+module.exports = router;
